1a88c032f1171f599ad12e46d9067c85c2859ba7
Impopite <andreaacciari.mc01@gmail.com>
2026-07-11 21:21:58 +0200

    feat: added item serializer

---
+++ api/src/main/java/it/impo/protect/api/utils/ItemSerializer.java
@@ -0,0 +1,37 @@
+package it.impo.protect.api.utils;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import java.io.IOException;
+
+public class ItemSerializer {
+
+    public static byte[] itemToBytes(ItemStack item) throws IOException {
+        if(item == null || item.getType().isAir()) return new byte[0];
+        return item.serializeAsBytes();
+    }
+
+    public static ItemStack itemFromBytes(byte[] bytes) throws IOException {
+        if(bytes == null || bytes.length == 0) return null;
+        return ItemStack.deserializeBytes(bytes);
+    }
+
+    public static byte[] safeItemToBytes(ItemStack item, Plugin plugin) {
+        try{
+            return itemToBytes(item);
+        }catch(IOException e){
+            plugin.getLogger().warning("Error while trying to deserialize item to bytes: " + e);
+            return new byte[0];
+        }
+    }
+
+    public static ItemStack safeItemFromBytes(byte[] bytes, Plugin plugin){
+        try{
+            return itemFromBytes(bytes);
+        }catch(IOException e){
+            plugin.getLogger().warning("Error while trying to deserialize item from bytes: " + e);
+            return null;
+        }
+    }
+}
